e297d76352fb98539eee6096d03564b8218eec79
AmirHossein <amiryousefi98@gmail.com>
2020-08-16 16:39:17 +0430

    Log Class : Simple C++ class

--- Class/LogClass/Main.cpp
+++ Class/LogClass/Main.cpp
@@ -1,5 +1,44 @@
 #include<iostream>
 
+class Log {
+//public variable of class
+public :
+    const int LogLevelError = 0;
+    const int LogLevelWarning = 1;
+    const int LogLevelInfo = 2;
+//private variable of class
+private:
+    int m_LogLevel = LogLevelInfo;
+//public function of class
+public :
+    void Warn(const char* message){
+        if (m_LogLevel >= LogLevelWarning)
+            std::cout << "[WARNING]:" << message << std::endl;
+    }
+    void Error(const char* message){
+        if (m_LogLevel >= LogLevelError)
+            std::cout << "[ERROR]:" << message << std::endl;
+    }
+    void Info(const char* message){
+        if (m_LogLevel >= LogLevelInfo)
+            std::cout << "[INFO]:" << message << std::endl;
+    }
+    void SetLevel(int level){
+        m_LogLevel = level;
+    }
+
+};
+
 int main(){
-    
+    Log log;
+    //LogLevelWarning -> warning and Error printed
+    log.SetLevel(log.LogLevelWarning);
+    // log.Error("Hello!");
+    // log.Warn("Hello!");
+    // log.Info("Hello!");
+    //LogLevelError -> Just  Error printed
+    log.SetLevel(log.LogLevelError);
+    log.Error("Hello!");
+    log.Warn("Hello!");
+    log.Info("Hello!");
 }
