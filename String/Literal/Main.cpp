38ed5610b9dc9978fd7fcde8a0d1fb4442f8b4fe
AmirHossein <amiryousefi98@gmail.com>
2020-08-31 18:39:50 +0430

    string

--- String/Literal/Main.cpp
+++ String/Literal/Main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 #include<string>
+#include<cstring>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-    "amirhossein";
+    //we have 11 char in this string literal 
+    //but shows 12 because the null str added to last
+    const char name[12] = "amirhossein";
+    const char name_two [13] = "Yousefi\0asem";
+    //in C include stdlib
+    //in C++ include cstring
+    std::cout << strlen(name_two) << std::endl;
+    //we see the output is 7 because \0 placed in 8th place
+    char* name_three = "Amirhossein";
+    //name_three[2] = 'a';
+    std::cout << name_three << std::endl;
+
+ 
 }
